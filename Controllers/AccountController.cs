174dae05afa2e7bce0a67d0b13c0cf72af2b5f85
Reshma Sivaramasubramanian <reshusuresh5@gmail.com>
2021-10-13 22:39:04 +0530

    Authentication without Identity

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Demo.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+
+        private readonly DemoContext _demoContext;
+
+        public AccountController(DemoContext demoContext)
+        {
+            _demoContext = demoContext;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel registerModel)
+        {
+            
+                if (ModelState.IsValid)
+                {
+                    var check = _demoContext.Register.FirstOrDefault(s => s.UserEmail == registerModel.UserEmail);
+                    if (check == null)
+                    {
+                        registerModel.Password = registerModel.Password;
+                   
+                        _demoContext.Register.Add(registerModel);
+                        _demoContext.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.error = "Email already exists";
+                        return View();
+                    }
+
+
+                }
+                return View();
+
+
+            
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = password;
+                var data = _demoContext.Register.Where(s => s.UserEmail.Equals(email) && s.Password.Equals(password)).ToList();
+                if (data.Count() > 0)
+                {
+
+                    ViewData["message"] = "Successfully Logged In";
+                    return RedirectToAction("Index","Home", null);
+
+
+                }
+                else
+                {
+                    ViewData["message"] = "Login Failed";
+                    return RedirectToAction("Login","Account", null);
+                }
+            }
+            return View();
+        }
+
+       
+
+    }
+}
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public class RegisterModel
+    {
+        [Key]
+        [Required(ErrorMessage = "Please Enter User Name")]
+        [Display(Name ="UserName :")]
+        public string User_Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email")]
+        [Display(Name = "Email :")]
+        public string UserEmail { get; set; }
+
+    [Required(ErrorMessage = "Please Enter Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password :")]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Conform Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Conform Password :")]
+        [Compare("Password")]
+        public string Conform_Pwd { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Gender")]
+        [Display(Name = "Gender :")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Martial_Status")]
+        [Display(Name = "Martial Statusv:")]
+        public string Martial_Status { get; set; }
+
+       
+
+
+
+    }
+}
